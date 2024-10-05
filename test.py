6633341357689b3cfa34b1b10e86f34570c42b50
Seb_Programming <a01412395@tec.mx>
2024-10-05 17:05:25 -0600

    agregar pestaña

--- test.py
+++ test.py
@@ -4,5 +4,13 @@ from time import sleep
 # inicializar pygame
 pygame.init()
 
+# Dimensiones de la ventana
+ANCHO = 800
+ALTO = 600
+pantalla = pygame.display.set_mode((ANCHO, ALTO))
+
+# Título y icono
+pygame.display.set_caption("Exoplanet for Beginners!")
+
 def lol():
     pass
