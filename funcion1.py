3939c7f7c7db71ff155ec7045bda509b919e051d
Satanitan <a01410023@tec.mx>
2024-10-05 17:37:05 -0600

    aqui va la primera funcion

--- funcion1.py
+++ funcion1.py
@@ -1,1 +1,11 @@
-# aquí va la primera función
+import tkinter as tk
+from tkinter import PhotoImage
+
+def crear_boton(ventana):
+    button_image = PhotoImage(file="ruta/a/tu_imagen.png")
+    start_button = tk.Button(ventana, image=button_image, command=start_game)
+    start_button.image = button_image  # Mantener referencia a la imagen
+    start_button.pack(pady=20)
+    
+    def start_game():
+        print("juega bru")
