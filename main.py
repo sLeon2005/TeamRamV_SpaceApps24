160985c79fa547a957ea54d983a1d2f211b5cda5
Seb_Programming <a01412395@tec.mx>
2024-10-05 18:39:54 -0600

    paneles y main

--- main.py
+++ main.py
@@ -16,7 +16,22 @@ pygame.display.set_caption("Exoplanets for Beginners!")
 intro_imagen = pygame.image.load("images\intro.jpeg")
 intro_imagen = pygame.transform.scale(intro_imagen, DIMENSIONES)
 
-while True:
-    pantalla.blit(bg, (0,0))
+start_imagen = pygame.image.load("images\start.png")
+
+# start_imagen.
+
+loop = True
+while loop:
+    # checar si se cerró la ventana
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            loop = False
+
+    # poner imagen de fondo
+    pantalla.blit(intro_imagen, (0,0))
+    # poner botón de start
+    pantalla.blit(start_imagen, ((ANCHO-start_imagen.get_width())/2,360))
+
+    # while start_imagen.
+
     pygame.display.update()
-    sleep(1)
